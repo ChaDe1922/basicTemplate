104ec601dd3ae1348981a4b0855d354d410297a5
Chalece DeLaCoudray <dahliablack22@gmail.com>
2019-02-26 09:59:12 -0500

    Initial commit

---
+++ js/sceneMain.js
@@ -0,0 +1,22 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload()
+    {
+        //this is where we load our assets, like images and sounds, before they need to be used
+
+     
+    	
+    }
+    create() {
+        //this is where we define objects, like sharks and bubbles, to be used in the game
+        
+       
+    }
+    update() 
+    {
+        //this is a constant loop. Place things that need to be checked over and over.
+        
+    }
+}
