38251f8032e3ba72bff1b82a26ec45f786d72053
Chalece DeLaCoudray <dahliablack22@gmail.com>
2019-03-26 02:03:58 -0400

    Beginning Sound
    Sound Constant

--- js/constants.js
+++ js/constants.js
@@ -5,5 +5,6 @@ class Constants
             this.SET_SCORE = "setScore";   //constant in uppercase
             this.UP_POINTS = "upPoints";
             this.SCORE_UPDATED="scoreUpdated";
+            this.PLAY_SOUND="playSound";
         }
     }
